0a1a55bbba39e0f97bebdded1b2caf7e358450e4
KATHIRAVAN-GITHUB <kathirkabilan007@gmail.com>
2025-03-11 21:56:11 +0530

    Added redis

--- src/main/java/com/example/Titan/Service/ProductCardService.java
+++ src/main/java/com/example/Titan/Service/ProductCardService.java
@@ -4,6 +4,9 @@ import com.example.Titan.Model.ProductCardModel;
 import com.example.Titan.Repository.ProductCardRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,18 +14,22 @@ public class ProductCardService {
   @Autowired
   private ProductCardRepository productCardRepository;
 
+  @CachePut(value = "products", key = "#product.id")
   public ProductCardModel saveProduct(ProductCardModel product) {
     return productCardRepository.save(product);
   }
 
+  @Cacheable(value = "allProducts")
   public List<ProductCardModel> getAllProducts() {
+    System.out.println("Fetching all products from database...");
     return productCardRepository.findAll();
   }
 
+  @Cacheable(value = "products", key = "#id")
   public ProductCardModel getProductById(Long id) {
+    System.out.println("Fetching from database...");
     return productCardRepository.findById(id).orElse(null);
   }
-
 
 
 }
